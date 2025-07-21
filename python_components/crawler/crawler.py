0f841f884cf802d42ce95fc8d3b70dfdae3b8b9b
Ray Bell <rayjohnbell0@gmail.com>
2025-07-20 22:27:34 -0400

    Update crawler.py

--- python_components/crawler/crawler.py
+++ python_components/crawler/crawler.py
@@ -10,7 +10,7 @@
 #     "tqdm>=4.67.1",
 # ]
 # ///
-# Example: uv run crawler.py https://www.python.org output.csv
+# Example: uv run https://raw.githubusercontent.com/raybellwaves/asap_pdf/refs/heads/uv-inline-deps/python_components/crawler/crawler.py https://www.python.org output.csv
 import argparse
 import csv
 import io
